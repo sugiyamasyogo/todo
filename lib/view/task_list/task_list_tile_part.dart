68d87b1663423c60ace556c6e4b59076a8b55624
sugiyamasyogo <sugiyamasyogo27@gmail.com>
2022-02-01 09:52:09 +0900

    ListTileのカスタムウィジェット化と期日表記を日本式に修正

---
+++ lib/view/task_list/task_list_tile_part.dart
@@ -0,0 +1,20 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:todo/data/task.dart';
+import 'package:todo/util/functions.dart';
+
+class TaskListTilePart extends StatelessWidget {
+  final Task task;
+
+   TaskListTilePart({Key? key , required this.task}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(task.title),
+      subtitle: AutoSizeText(
+          convertDateTimeToString(task.limitDateTime),
+      ),
+    );
+  }
+}
