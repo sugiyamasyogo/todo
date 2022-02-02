93e98bc45d0053e3526bf1b9e79d256fd3f6c2fd
sugiyamasyogo <sugiyamasyogo27@gmail.com>
2022-02-02 14:53:31 +0900

    PopupMenuButtonの実装

--- lib/view/task_list/task_list_tile_part.dart
+++ lib/view/task_list/task_list_tile_part.dart
@@ -5,7 +5,7 @@ import 'package:todo/util/constants.dart';
 import 'package:todo/util/functions.dart';
 import 'package:todo/view/style.dart';
 
-class TaskListTilePart extends StatelessWidget {
+class TaskListTilePart extends StatefulWidget {
   final Task task;
   final ValueChanged onFinishChanged;
   final VoidCallback onDelete;
@@ -20,71 +20,91 @@ class TaskListTilePart extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<TaskListTilePart> createState() => _TaskListTilePartState();
+}
+
+class _TaskListTilePartState extends State<TaskListTilePart> {
+  bool isDisplayPopupMenu = false;
+
+  @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Radio(
-        value: true,
-        groupValue: task.isFinished,
-        onChanged: (value) => onFinishChanged(value),
-      ),
-      onTap: onEdit,
-      onLongPress: onDelete,
-      title: Row(
-        children: [
-          (task.isImportant)
-              ? Padding(
-            padding: const EdgeInsets.only(right: 4.0),
-            child: Container(
-              padding: EdgeInsets.all(4.0),
-              color: Colors.black,
-              child: Text(
-                StringR.important,
-                style: TextStyles.listTileChipTextStyle,
+    return MouseRegion(
+      onHover: (event) {
+        print("onHover");
+        setState(() {
+          isDisplayPopupMenu = true;
+        });
+      },
+      onExit: (_) {
+        print("onExit");
+        setState(() {
+          isDisplayPopupMenu = false;
+        });
+      },
+      child: ListTile(
+        leading: Radio(
+          value: true,
+          groupValue: widget.task.isFinished,
+          onChanged: (value) => widget.onFinishChanged(value),
+        ),
+        onTap: widget.onEdit,
+        onLongPress: widget.onDelete,
+        title: Row(
+          children: [
+            (widget.task.isImportant)
+                ? Padding(
+                    padding: const EdgeInsets.only(right: 4.0),
+                    child: Container(
+                      padding: EdgeInsets.all(4.0),
+                      color: Colors.black,
+                      child: Text(
+                        StringR.important,
+                        style: TextStyles.listTileChipTextStyle,
+                      ),
+                    ),
+                  )
+
+                ///Chip Widgetでの実装
+                // ? Padding(
+                //   padding: const EdgeInsets.all(4.0),
+                //   child: Chip(
+                //       label: Text(StringR.important),),
+                // )
+                : Container(),
+            Expanded(
+              child: AutoSizeText(
+                widget.task.title,
+                overflow: TextOverflow.ellipsis,
               ),
             ),
-          )
-
-          ///Chip Widgetでの実装
-          // ? Padding(
-          //   padding: const EdgeInsets.all(4.0),
-          //   child: Chip(
-          //       label: Text(StringR.important),),
-          // )
-              : Container(),
-          Expanded(
-            child: AutoSizeText(
-              task.title,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-        ],
-      ),
-      subtitle: AutoSizeText(
-        convertDateTimeToString(task.limitDateTime),
-      ),
-      trailing: PopupMenuButton(
-          tooltip: StringR.showMenu,
-          icon: Icon(Icons.more_vert),
-          itemBuilder: (context) {
-            return [
-              PopupMenuItem<TaskListTileMenu>(
-                child: Text(StringR.edit),
-                value: TaskListTileMenu.EDIT,
+          ],
+        ),
+        subtitle: AutoSizeText(
+          convertDateTimeToString(widget.task.limitDateTime),
+        ),
+        trailing: PopupMenuButton(
+                tooltip: StringR.showMenu,
+                icon: (isDisplayPopupMenu) ? Icon(Icons.more_vert) : Container(),
+                itemBuilder: (context) {
+                  return [
+                    PopupMenuItem<TaskListTileMenu>(
+                      child: Text(StringR.edit),
+                      value: TaskListTileMenu.EDIT,
+                    ),
+                    PopupMenuItem<TaskListTileMenu>(
+                      child: Text(StringR.delete),
+                      value: TaskListTileMenu.DELETE,
+                    ),
+                  ];
+                },
+                onSelected: (selectedMenu) {
+                  if (selectedMenu == TaskListTileMenu.EDIT) {
+                    widget.onEdit();
+                  } else {
+                    widget.onDelete();
+                  }
+                },
               ),
-              PopupMenuItem<TaskListTileMenu>(
-                child: Text(StringR.delete),
-                value: TaskListTileMenu.DELETE,
-              ),
-            ];
-          },
-          onSelected: (selectedMenu) {
-            if (selectedMenu == TaskListTileMenu.EDIT) {
-              onEdit();
-            } else {
-              onDelete();
-            }
-
-    },
       ),
     );
   }
